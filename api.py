ff85b927b3994ef6a548e3935292bc3cee03ca6b
Adarsh Sahu <adarshsahu2510@gmail.com>
2025-06-12 17:03:26 +0530

    .

--- api.py
+++ api.py
@@ -56,12 +56,12 @@ face_analyzer = FaceAnalysis(
     name='buffalo_l',
     root=MODEL_DIR,
     download=False,
-    providers=preferred_providers,
+    providers=preferred_providers[0],
     allowed_modules=["detection", "recognition", "landmark_2d_106", "landmark_3d_68"]
 )
 face_analyzer.prepare(ctx_id=0, det_size=(640, 640))
 
-swapper = get_model(INSWAPPER_PATH, providers=preferred_providers)
+swapper = get_model(INSWAPPER_PATH, providers=preferred_providers[0])
 
 gfpgan = GFPGANer(
     model_path=GFPGAN_PATH,
@@ -69,7 +69,7 @@ gfpgan = GFPGANer(
     arch='clean',
     channel_multiplier=2,
     bg_upsampler=None,
-    device='cuda' if 'CUDAExecutionProvider' in preferred_providers else 'cpu'
+    device='cuda' if 'CUDAExecutionProvider' in preferred_providers[0] else 'cpu'
 )
 
 # ---- FastAPI Models ----
